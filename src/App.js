cd2335f56ba9482546afbe60b627fbcdf54e203d
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-09-15 17:39:58 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Navbar, NavDropdown, Form, Nav, FormControl, Button, Container, Col } from 'react-bootstrap'
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
+  let [ login, setLogin] = useState(true);
+  let [ page, setPage] = useState(1);
+  let [ posts, setPosts] = useState([{
+    post_id: 1,
+    title: 'Firstaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    user: 'user',
+    date: '21/09/13',
+  }]);
   return (
     <div className="App">
     </div>
