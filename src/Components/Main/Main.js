e4ee2d283a0895032114d6e915bb1e63aac2928c
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-09-16 18:13:22 +0900

    [ADD]게시판 화면 반응형 추가

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -8,7 +8,7 @@ function Main(props) {
   let history = useHistory();
   return (
     <Container>
-      <Form className="d-flex">
+      <Form className="d-flex mb-3">
         <FormControl
           type="search"
           placeholder="Search"
@@ -17,33 +17,28 @@ function Main(props) {
         />
         <Button variant="outline-primary">Search</Button>
       </Form>
-      <ul class="d-flex justify-content-between list-group list-group-horizontal-sm">
-                <li class="list-group-item flex-fill">Title</li>
-                <li class="list-group list-group-horizontal-sm">
-                  <li class="list-group-item w-80">User</li>
-                  <li class="list-group-item">Date</li>
-                </li>
-              </ul>
-            <ListGroup horizontal className="d-flex justify-content-between">
-              <ListGroup.Item className="flex-fill">Title</ListGroup.Item>
-              <ListGroup horizontal className="d-flex justify-content-between">
-                <ListGroup.Item>User</ListGroup.Item>
-                <ListGroup.Item>Date</ListGroup.Item>
-              </ListGroup>
-            </ListGroup>
-          {
-            props.posts.map((post) =>
-            {return (
-              <ListGroup horizontal className="d-flex justify-content-between" onClick={ () => {history.push(`/posts/${post.number}`)}}>
-              <ListGroup.Item className="flex-fill"><p className="title d-block">{post.title}</p></ListGroup.Item>
-              <ListGroup horizontal className="d-flex justify-content-between">
-                <ListGroup.Item>{post.user}</ListGroup.Item>
-                <ListGroup.Item>{post.date}</ListGroup.Item>
-              </ListGroup>
-            </ListGroup>
-            )})
-          }
-    </Container>
+      <div className="posts">
+      <ListGroup horizontal className="d-flex justify-content-between lists mt-1">
+        <ListGroup.Item className="flex-fill list">Title</ListGroup.Item>
+        <ListGroup horizontal className="d-flex justify-content-between list">
+          <ListGroup.Item className="user list">User</ListGroup.Item>
+          <ListGroup.Item className="date list">Date</ListGroup.Item>
+        </ListGroup>
+      </ListGroup>
+      {
+        props.posts.map((post) =>
+        {return (
+        <ListGroup horizontal className="d-flex justify-content-between lists mt-1" onClick={ () => {history.push(`/posts/${post.number}`)}}>
+          <ListGroup.Item className="flex-fill list"><p className="title d-block">{post.title}</p></ListGroup.Item>
+          <ListGroup horizontal className="d-flex justify-content-between list">
+            <ListGroup.Item className="user list">{post.user}</ListGroup.Item>
+            <ListGroup.Item className="date list">{post.date}</ListGroup.Item>
+          </ListGroup>
+        </ListGroup>
+        )})
+      }
+      </div>
+      </Container>
   );
 }
 
